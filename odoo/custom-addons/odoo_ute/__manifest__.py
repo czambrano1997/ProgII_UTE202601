5f75f48ba10a219e634f53109626ec06733e191d
czambrano1997 <ceiderz1997@gmail.com>
2026-04-28 10:54:49 -0500

    Práctica de herencia de modelo y vista en odoo_ute

--- odoo/custom-addons/odoo_ute/__manifest__.py
+++ odoo/custom-addons/odoo_ute/__manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name': "odoo_ute",
+    'name': "Administración de la UTE",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Gestión de docentes y estudiantes de la UTE",
 
     'description': """
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Ceider Zambrano",
+    'website': "https://gezaforge.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -17,17 +17,18 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        #'views/views.xml',
+        #'views/templates.xml',
+        'views/sale_order.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        #'demo/demo.xml',
     ],
 }
 
---
+++ odoo/custom-addons/odoo_ute/models/sale_order.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    deliver_detailsx = fields.Text(string="Detalles")
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
