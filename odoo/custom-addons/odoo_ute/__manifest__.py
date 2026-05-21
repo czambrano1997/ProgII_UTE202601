11cf30d8208881931fe48c351d86b4ace67f6824
Estudiante UTE <estudiante@ute.edu.ec>
2026-05-21 04:13:54 -0500

    modificacion

--- odoo/custom-addons/odoo_ute/__manifest__.py
+++ odoo/custom-addons/odoo_ute/__manifest__.py
@@ -1,45 +1,45 @@
-{
-    'name': "Administración de la UTE",
+    {
+        'name': "Administración de la UTE",
 
-    'summary': "Gestión de docentes y estudiantes de la UTE",
+        'summary': "Gestión de docentes y estudiantes de la UTE",
 
-    'description': """
-	Funcionalidades del módulo de gestión de la UTE.
-    """,
+        'description': """
+        Funcionalidades del módulo de gestión de la UTE.
+        """,
 
-    'author': "Ceider Zambrano",
-    'website': "https://gezaforge.com",
-    'license': 'LGPL-3',
+        'author': "Ceider Zambrano",
+        'website': "https://gezaforge.com",
+        'license': 'LGPL-3',
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Education',
-    'version': '0.1',
-    'application': True,
+        # Categories can be used to filter modules in modules listing
+        # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+        # for the full list
+        'category': 'Education',
+        'version': '0.1',
+        'application': True,
 
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'mail'],
+        # any module necessary for this one to work correctly
+        'depends': ['base', 'mail'],
 
-    # always loaded
-    'data': [
-        'security/ute_group.xml',
-        'security/ir.model.access.csv',
-        'security/ute_rule.xml',
-        'views/teacher.xml',
-        'views/signature.xml',
-        'views/universidad_carrera_views.xml',
-        'views/universidad_profesor_views.xml',
-        'views/universidad_materia_views.xml',
-        'views/universidad_estudiante_views.xml',
-        'views/universidad_matricula_views.xml',
-        'views/menu.xml',
-        'views/report_universidad_estudiante.xml',
-        'data/demo.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        #'demo/demo.xml',
-    ],
-}
+        # always loaded
+        'data': [
+            'security/ute_group.xml',
+            'security/ir.model.access.csv',
+            'security/ute_rule.xml',
+            'views/teacher.xml',
+            'views/signature.xml',
+            'views/universidad_carrera_views.xml',
+            'views/universidad_profesor_views.xml',
+            'views/universidad_materia_views.xml',
+            'views/universidad_estudiante_views.xml',
+            'views/universidad_matricula_views.xml',
+            'views/menu.xml',
+            'views/report_universidad_estudiante.xml',
+            'data/demo.xml',
+        ],
+        # only loaded in demonstration mode
+        'demo': [
+            #'demo/demo.xml',
+        ],
+    }
 
