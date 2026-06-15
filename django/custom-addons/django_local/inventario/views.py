8d245623417a9b7fe0e535c8a97f2e97d0ba4fe1
czambrano1997 <ceiderz1997@gmail.com>
2026-06-15 07:48:20 -0500

    Aplicación de relaciones entre modelos en Django

--- django/custom-addons/django_local/inventario/views.py
+++ django/custom-addons/django_local/inventario/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from .models import Categoria
+from .models import Categoria, Producto
 
-# Create your views here.
+# Funciones para vista categoria
 def lista_categoria(request):
     categorias = Categoria.objects.all()
-    return render(request, 'inventario/categoria.html', {'categorias': categorias, 'carrera': 'PROGRAMACION II'})
+    contexto = {
+        'carrera': 'PROGRAMACION II',
+        'categorias': categorias,
+        'total': categorias.count(),
+    }
+    return render(request, 'inventario/categoria.html', contexto)
+
+# Funciones para vista productos
+def lista_productos(request):
+    productos = (
+        Producto.objects
+        .select_related('categoria', 'ficha_tecnica')  # FK y 1:1
+        .prefetch_related('proveedores')                # N:M
+        .all()
+    )
+    return render(request, 'inventario/producto.html', {'productos': productos})
