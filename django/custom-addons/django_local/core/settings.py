0eb0ebd16291213c5981a9e02ea4c963ad06b89e
AKsojuu <mathiasmath000@gmail.com>
2026-06-15 02:10:52 -0500

    Creacion de inventario categoria y mejora de vista html

--- django/custom-addons/django_local/core/settings.py
+++ django/custom-addons/django_local/core/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_lab',
         'USER': 'django',
-        'PASSWORD': 'djangoAdmin',
+        'PASSWORD': 'djangoadmin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
