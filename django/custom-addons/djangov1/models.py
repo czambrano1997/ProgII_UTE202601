0693b1f1c7cf77a79f7730198443b4b794621e6b
mc7669662 <mc7669662@gmail.com>
2026-06-17 08:57:09 -0500

    Creacion de los modulos

--- django/custom-addons/djangov1/models.py
+++ django/custom-addons/djangov1/models.py
@@ -1,8 +1,55 @@
 from django.db import models
 
 # Create your models here.
+# Modelo Clientes
 class clientes(models.Model):
     nombre = models.CharField(max_length=100)
+    s_nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
+    s_apellido = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    telefono = models.IntegerField(verbose_name="Ingresa el telefono")
     fecha_registro = models.DecimalField(auto_created=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+# Modelo Reservas
+class Reservas(models.Model):
+    cliente  = models.ForeignKey('clientes' , on_delete=models.CASCADE)
+    numero_Rs = models.AutoField(primary_key=True)
+    Hora_ll = models.TimeField(auto_now= False,
+                                 auto_now_add= False,
+                                 null = False,
+                                 blank= False,
+                                 verbose_name="Hora de llegada"
+                                 )
+    Hora_Sa = models.TimeField(auto_now= False,
+                               auto_now_add= False,
+                               null= False,
+                               blank= False,
+                               verbose_name="Hora de salida")
+    Pago = models.DecimalField(max_digits= 10,
+                               decimal_places= 2,)
+    cantidad_ju = models.IntegerField(verbose_name="Numero de jugadores")
+    paquete_incluido = models.ForeignKey('paquete' , on_delete=models.CASCADE,
+                                            null= True,
+                                            blank= True)
+    
+    nota = models.TextField(verbose_name="Nota extra de la reservacion")
+
+    def __str__(self):
+        return f"Reservado por :{self.cliente}Numero de reserva :{self.numero_Rs}"
+    
+# Modelo paquete
+class paquete(models.Model):
+    nombre_p = models.CharField(max_length=100)
+    costo_p =  models.DecimalField(max_digits=10,
+                                   decimal_places=2,
+                                   verbose_name="Costo del paquete")
+    descripcion = models.TextField(verbose_name="Descripcion del paquete")
+
+    def __str__(self):
+        return self.nombre_p
+#Modelo invetario
+class inventario(models.Model):
