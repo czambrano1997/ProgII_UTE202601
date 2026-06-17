966cc8aba490e782ac7c6aebe46de4372c984b74
Este-fania <estefaniaa.7@outlook.com>
2026-06-17 03:29:30 -0500

    modificaciones del proyecto

--- django/custom-addons/django_local/core/settings.py
+++ django/custom-addons/django_local/core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_lab',
         'USER': 'django',
-        'PASSWORD': 'djangoAdmin',
+        'PASSWORD': 'djangoadmin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
