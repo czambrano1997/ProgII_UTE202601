9dee15a00fed5e081cecbb97f05896ae2979366f
ironplisquin2 <matyvelastegui2006@gmail.com>
2026-06-17 03:03:39 -0500

    migracion de sql y adelanto de proyecto

--- django/custom-addons/django_local/inventario/admin.py
+++ django/custom-addons/django_local/inventario/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import Categoria, Proveedor, FichaTecnica, Producto
+from django.contrib import admin
+from .models import Proveedor, Categoria, Producto, Cliente, RegistroVenta
 
-# Registro de modelos en apartado administración
-admin.site.register(Categoria)
-admin.site.register(Proveedor)
-admin.site.register(FichaTecnica)
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('nombre_empresa', 'email', 'activo')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'categoria', 'precio', 'existencias']
-    filter_horizontal = ['proveedores']  # widget visual para el M2M
+    list_display = ('nombre', 'precio_venta', 'stock_actual', 'categoria')
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cedula')
+
+@admin.register(RegistroVenta)
+class RegistroVentaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'producto', 'cliente', 'cantidad', 'total_pago')
