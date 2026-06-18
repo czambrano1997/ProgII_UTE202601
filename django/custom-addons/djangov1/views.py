916eeec9b700f94d45eea4331e07c9b600fc5cd6
mc7669662 <mc7669662@gmail.com>
2026-06-18 10:04:19 -0500

    Ingreso de las viwes

--- django/custom-addons/djangov1/views.py
+++ django/custom-addons/djangov1/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Clientes,Reservas,Inventario,Gestion_reservas,Paquete
 
 # Create your views here.
 
-def Clientes(request):
+def vista_Clientes(request):
     clientes = Clientes.objects.all()
     return render(request,'clientes.html',{'clientes': clientes})
+
+def vista_Reservas(request):
+    reservas = Reservas.objects.all()
+    return render(request,'resvervas.html',{'reservas':reservas})
