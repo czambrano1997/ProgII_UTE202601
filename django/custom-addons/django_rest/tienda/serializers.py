d0fdb0dccae32573e9c568f37dce38ce8ad7f8ac
alumnos <mathiasmath000@gmail.com>
2026-06-24 04:00:05 -0500

    Django Rest Framework ejemplo tienda

---
+++ django/custom-addons/django_rest/tienda/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Producto
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Producto
+        fields = [
+            'id',
+            'nombre',
+            'precio',
+            'stock',
+        ]
+
+    def validate_precio(self, value):
+        if value<=0:
+            raise serializers.ValidationError('Debe ser >0')
+        return value
---
+++ django/custom-addons/django_rest/tienda/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Producto
+from .serializers import ProductoSerializer
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset=Producto.objects.all()
+    serializer_class=ProductoSerializer
+    permission_classes=[
+        IsAuthenticatedOrReadOnly
+    ]
+
+# Create your views here.
