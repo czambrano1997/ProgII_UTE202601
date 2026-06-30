adbd25b49dd43cfe7b7909c0f384dda882948aa3
gomez_danny <dannygomez20071@gmail.com>
2026-06-30 05:54:59 -0500

    guardado

--- django/custom-addons/django_rest_local/tienda/tests.py
+++ django/custom-addons/django_rest_local/tienda/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Producto 
+class ProductoModelTestCase(TestCase):
+    def setUp(self):
+        self.p = Producto.objects.create(
+            nombre='Laptop',precio=89.99,stock=5
+        )
+
+def test_nombre_producto(self):
+        self.assertEqual(self.p.nombre, 'Laptop')
 
 # Create your tests here.
