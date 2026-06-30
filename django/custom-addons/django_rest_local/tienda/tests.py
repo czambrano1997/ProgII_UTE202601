7e1701b26e500ad9931e78bdafbe526c27fbc067
luisar2307 <luisandresarias2007@gmail.com>
2026-06-30 05:50:37 -0500

    se sube carpeta test

--- django/custom-addons/django_rest_local/tienda/tests.py
+++ django/custom-addons/django_rest_local/tienda/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from .models import Producto
 
-# Create your tests here.
+# Pruebas Unitarias
+class ProductoModelTest(TestCase):
+    def setUp(self):
+        self.p=Producto.objects.create(
+            nombre='Laptop',precio=999,stock=5)
+        
+    def test_nombre_correcto(self):
+        self.assertEqual(self.p.nombre,'Laptop')
+
+# Pruebas funcionales
+
+class ProductoViewTest(TestCase):
+    def setUp(self):
+        self.client = Client
