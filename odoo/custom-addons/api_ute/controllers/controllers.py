4ff50bc0f60b75743d1541b72574a49563b51614
czambrano1997 <ceiderz1997@gmail.com>
2026-07-20 08:26:41 -0500

    Se crea módulo en odoo para gestión de API mediante el uso del controller en Odoo

---
+++ odoo/custom-addons/api_ute/controllers/controllers.py
@@ -0,0 +1,47 @@
+from odoo import http
+from odoo.http import request
+import json
+
+class ApiUte(http.Controller):
+    @http.route('/api_ute/hello', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+
+    @http.route('/api_ute/signature/all', type='http', auth='public', methods=['GET', 'POST'], csrf=False)
+    def list_signatures(self, **kw):
+        signatures_data = request.env['ou.signature'].sudo().search_read([], ['id', 'name'])
+        return request.make_response(
+            json.dumps({'signatures': signatures_data}),
+            headers=[('Content-Type', 'application/json')]
+        )
+
+    
+    @http.route('/api_ute/signature/create', type='http', auth='public', methods=['POST'], csrf=False)
+    def create_signatures(self, **kw):
+        print("create_signatures")
+        data = json.loads(request.httprequest.data)
+        print(data)
+        new_record = request.env['ou.signature'].sudo().create({
+            'name': data['name']
+        })
+        return request.make_response(json.dumps({'status': 'success', 'new_record': new_record.id}), 
+                                         headers=[('Content-Type', 'application/json')])
+        # data = request.httprequest
+        # print(data)
+        # signatures_data = request.env['ou.signature'].sudo().search_read([], ['id', 'name'])
+        # return request.make_response(
+        #     json.dumps({'signatures': signatures_data}),
+        #     headers=[('Content-Type', 'application/json')]
+        # )
+
+        # return http.request.render('api_ute.listing', {
+        #     'root': '/api_ute/api_ute',
+        #     'objects': http.request.env['api_ute.api_ute'].search([]),
+        # })
+
+#     @http.route('/api_ute/api_ute/objects/<model("api_ute.api_ute"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('api_ute.object', {
+#             'object': obj
+#         })
+
